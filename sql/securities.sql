567139d1f83a3b332f227e4bc09e0c631e7d9b54
Rachana P Bennur <rachanapbennur@gmail.com>
2022-10-21 11:18:57 +0530

    sql commands added for security tabl

---
+++ sql/securities.sql
@@ -0,0 +1,55 @@
+INSERT INTO securities  (ticker,security_name,current_market_price,asset_class) VALUES ('IBM','International Business Machines Corp',126.7,'International Market Stocks');
+INSERT INTO securities  (ticker,security_name,current_market_price,asset_class) VALUES ('CRM','Salesforce Inc',156.12,'Corporate Stocks');
+INSERT INTO securities  (ticker,security_name,current_market_price,asset_class) VALUES ('VZ','Verizon Communications Inc',37.05,'Small Cap Company Stocks');
+INSERT INTO securities  (ticker,security_name,current_market_price,asset_class) VALUES ('CVX','Chevron Corp',168.49,'main index stocks');
+
+
+
+INSERT INTO securities  (ticker,security_name,current_market_price,asset_class) VALUES ('BA','Boeing Co',138.50,'Government Stocks');
+INSERT INTO securities  (ticker,security_name,current_market_price,asset_class) VALUES ('V','Visa Inc',187,'main index stocks');
+INSERT INTO securities  (ticker,security_name,current_market_price,asset_class) VALUES ('INTC','Intel Corp',25.86,'Small Cap Company Stocks');
+INSERT INTO securities  (ticker,security_name,current_market_price,asset_class) VALUES ('WMT','Walmart Inc',133.55,'International Market Stocks');
+
+
+
+INSERT INTO securities  (ticker,security_name,current_market_price,asset_class) VALUES ('UNH','UnitedHealth Group Inc',519,'Government Stocks');
+INSERT INTO securities  (ticker,security_name,current_market_price,asset_class) VALUES ('WBA','Walgreens Boots Alliance Inc',32.32,'Small Cap Company Stocks');
+INSERT INTO securities  (ticker,security_name,current_market_price,asset_class) VALUES ('AMGN','Amgen Inc',247.45,'International Market Stocks');
+INSERT INTO securities  (ticker,security_name,current_market_price,asset_class) VALUES ('GS','Goldman Sachs Group Inc',310.10,'Corporate Stocks');
+
+
+
+
+INSERT INTO securities  (ticker,security_name,current_market_price,asset_class) VALUES ('MCD','McDonald''s Corp.',249.50,'International Market Stocks');
+INSERT INTO securities  (ticker,security_name,current_market_price,asset_class) VALUES ('DIS','Walt Disney Co',98,'International Market Stocks');
+INSERT INTO securities  (ticker,security_name,current_market_price,asset_class) VALUES ('CSCO','Cisco Systems Inc',41.45,'Small Cap Company Stocks');
+INSERT INTO securities  (ticker,security_name,current_market_price,asset_class) VALUES ('MRK','Merck and Co Inc',92.97,'main index stocks');
+
+
+
+INSERT INTO securities  (ticker,security_name,current_market_price,asset_class) VALUES ('DOW','Dow Inc.',44.82,'Government Stocks');
+INSERT INTO securities  (ticker,security_name,current_market_price,asset_class) VALUES ('HON','Honeywell International Inc',176.21,'International Market Stocks');
+INSERT INTO securities  (ticker,security_name,current_market_price,asset_class) VALUES ('MMM','3M Co',112.85,'Small Cap Company Stocks');
+INSERT INTO securities  (ticker,security_name,current_market_price,asset_class) VALUES ('KO','Coca-Cola Co.',55.07,'main index stocks');
+
+
+
+
+INSERT INTO securities  (ticker,security_name,current_market_price,asset_class) VALUES ('TRV','Travelers Cos Inc',169.40,'International Market Stocks');
+INSERT INTO securities  (ticker,security_name,current_market_price,asset_class) VALUES ('AXP','American Express Co',145.03,'Small Cap Company Stocks');
+INSERT INTO securities  (ticker,security_name,current_market_price,asset_class) VALUES ('NKE','Nike Inc. Cl B',86.55,'main index stocks');
+INSERT INTO securities  (ticker,security_name,current_market_price,asset_class) VALUES ('PG','Procter and Gamble Co',126.80,'Government Stocks');
+
+
+
+INSERT INTO securities  (ticker,security_name,current_market_price,asset_class) VALUES ('CAT','Caterpillar Inc',180.00,'International Market Stocks');
+INSERT INTO securities  (ticker,security_name,current_market_price,asset_class) VALUES ('HD','Home Depot Inc',268.75,'Small Cap Company Stocks');
+INSERT INTO securities  (ticker,security_name,current_market_price,asset_class) VALUES ('LRCX','Lam Research Corp',366.94,'Corporate Stocks');
+INSERT INTO securities  (ticker,security_name,current_market_price,asset_class) VALUES ('T','ATandT Inc',16.63,'Government Stocks');
+
+
+
+INSERT INTO securities  (ticker,security_name,current_market_price,asset_class) VALUES ('DGX','Quest Diagnostics Inc',134.70,'Corporate Stocks');
+INSERT INTO securities  (ticker,security_name,current_market_price,asset_class) VALUES ('LVS','Las Vegas Sands Corp',37.79,'Small Cap Company Stocks');
+INSERT INTO securities  (ticker,security_name,current_market_price,asset_class) VALUES ('ORCL','Oracle Corp',69.72,'main index stocks');
+INSERT INTO securities  (ticker,security_name,current_market_price,asset_class) VALUES ('CCL','Carnival Corp',7.92,'Government Stocks');

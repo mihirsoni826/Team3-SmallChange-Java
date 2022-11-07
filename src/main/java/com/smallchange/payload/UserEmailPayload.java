a6fb0d3f269708d9ac467a2f753e418b5b71be98
Soni, Mihir <mihirsoni826@gmail.com>
2022-11-07 11:03:33 +0530

    Additional controller changes for angular app integration

---
+++ src/main/java/com/smallchange/payload/UserEmailPayload.java
@@ -0,0 +1,14 @@
+package com.smallchange.payload;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserEmailPayload {
+    private String email;
+}
